87c92ed797960553e238ec382938adfcd7506293
Mischa <anton@exemple.com>
2025-03-04 21:57:59 +0100

    commit with the <Online commands> for cdf_mode

--- commands.py
+++ commands.py
@@ -1,3 +1,24 @@
 import time
 import random
 
+def cdf_online_mode(character):
+    command = input('Enter the code of character: ')  # ввод кода персонажа
+
+    if command == "code:123": # игрок
+        if character[0]:
+            character[0] = False
+            print('You are not online!')
+        else:
+            character[0] = True
+            print('You are online!')
+        return character
+    elif command == "code:456": # ии
+        if character[1]:
+            character[1] = False
+            print('AI are not online!')
+        else:
+            character[1] = True
+            print('AI are online!')
+        return character
+    else:
+        print('The code is not correct!')
--- main.py
+++ main.py
@@ -2,3 +2,26 @@ import commands
 import time
 import random
 
+cdf_mode = False
+cdf_online = False
+online_mode_characters = [False, True] # первое число игрок а второе ии
+
+print('Hello, it is the Hacker game! You need to enter the command "cdf.command.mode.on"')  # приветствие
+
+command = input('Enter the command: ')  # ввод команды
+
+if command == "cdf.command.mode.on":  # условие команды
+    cdf_mode = True
+
+while cdf_mode:  # цикл в режиме cdf_mode
+    command = input('Enter the cdf command: ')  # ввод команды
+
+    if command == "cdf.command.mode.off":  # обработка выхода из режима
+        cdf_mode = False
+    elif command == "cdf.command.online.on": # обработка режима онлайн
+        online_mode_characters = commands.cdf_online_mode(online_mode_characters)
+    else:
+        print('Unknown command, try again!')  # обработка неизвестной команды
+
+
+
